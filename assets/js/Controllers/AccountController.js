c90a10e3265ae0bce87124496d49ae73636c300e
Mary-Jane <maryjane_home@bobcat.home>
2015-11-21 23:03:49 +0000

    Created demo user account

---
+++ assets/js/Controllers/AccountController.js
@@ -0,0 +1,12 @@
+app.controller('AccountController',['$scope', function($scope) {
+    
+            $scope.id = 1;
+            $scope.name = "Annabelle Smith";
+            $scope.username = "a.smith85@hotmail.co.uk";
+            $scope.address = "1 Main Street";
+            $scope.city = "Belfast";
+            $scope.postcode = "BT1 2AB";
+            $scope.password = "password1";
+            $scope.credit = 1200;
+       
+}]);
