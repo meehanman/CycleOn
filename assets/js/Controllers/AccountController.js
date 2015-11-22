b71475e4507145905afc1c0daf5335eaf1f640d7
Dean Meehan <d3an.meehan@hotmail.com>
2015-11-22 15:08:12 +0000

    Added login and fixed some HTTP Errors.

--- assets/js/Controllers/AccountController.js
+++ assets/js/Controllers/AccountController.js
@@ -1,12 +1,12 @@
 app.controller('AccountController',['$scope', function($scope) {
-    
+
             $scope.id = 1;
             $scope.name = "Annabelle Smith";
-            $scope.username = "a.smith85@hotmail.co.uk";
+            $scope.username = "cycleon";
             $scope.address = "1 Main Street";
             $scope.city = "Belfast";
             $scope.postcode = "BT1 2AB";
-            $scope.password = "password1";
+            $scope.password = "password";
             $scope.credit = 1200;
-       
+
 }]);
--- assets/js/Controllers/NavController.js
+++ assets/js/Controllers/NavController.js
@@ -22,3 +22,4 @@ app.controller('NavController',['$scope', function($scope) {
         }
     ]
 }]);
+
